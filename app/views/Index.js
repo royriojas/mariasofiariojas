0a2f49efd2b1578b16baf914e27a1578a38e554c
Roy Riojas <royriojas@gmail.com>
2017-10-09 00:10:11 -0500

    FIX: change text for ms web

--- app/views/Index.js
+++ app/views/Index.js
@@ -7,20 +7,14 @@ const Index = ({ ...rest }) =>
       <div className="picture">
         <img src="./img/MariaSofia3.png" />
         <h1><a href="https://www.facebook.com/mariasofia.riojastaboada">María Sofía Riojas</a></h1>
-        <p>Mi pequeña hija. Esta página está dedicada a ti, dado que la familia de tu madre y tu madre solo nos dejan verte por una hora por semana. Nosotros, tu familia paterna, y yo tu padre te amamos mucho hija. Y esperamos de corazón que esto cambie pronto. Estamos entrampados en un montón de líos legales que ojalá a la larga nos permitan verte.</p>
-        <p>Tu no tienes la culpa hija, la intransigencia y tozudez de la familia de tu madre te daña y ellos no se dan cuenta.</p>
-        <p>Pero mi niña: No hay mal que dure cien años. Ni cuerpo que lo resista</p>
-        <p>Tu Padre que te ama.</p>
-        <p>Roy Riojas</p>
+
         <div className="ms-image" />
-        <p>Para ti pequeña Chofita... a quién amo con todo mi corazón... y no se me permite verte ni saber nada de ti hace más de un año</p>
-        <p>
-          La familia de tu madre te siente su propiedad, y no quieren que yo tenga contacto alguno contigo, ni siquiera aceptan los regalos que te llevé.
-          Sólo les preocupa saber cuánto dinero me podrán sacar como parte del juicio por alimentos que me han entablado.
-        </p>
-        <p>Me exigen una suma exhorbitante, sin embargo no permiten que ni mi familia ni yo tengamos contacto contigo... Yo te paso una cantidad acorde a tu edad, aparte de un seguro particular con Rimac para tu salud, y conforme vayas necesitando más veré de hacerte llegar más</p>
-        <p>Mi Pequeña Chofita, ésta página es para ti, para que un día la veas y sepas que contrario a lo que te puedan decir, tanto yo como mi familia solo tenemos amor para ti... y te extrañamos y necesitamos muchísimo.</p>
-        <p>Vamos a iniciar todas las acciones legales del caso, para poder tenerte nuevamente cerca mi bebita bella.</p>
+        <p>Para ti pequeña Chofita, a quién amo con todo mi corazón, una página hecha por tu papá, para tí.</p>
+        <p>Como testimonio del amor que te tengo, a pesar de los problemas que hemos tenido para poder verte</p>
+        <p>Te amo mucho hijita</p>
+        <p>Tu padre que te ama</p>
+        <p>&nbsp;</p>
+        <p>Roy</p>
       </div>
     </div>
   </Layout>;
